e7f39beb1d5a452c600e6819de3af8998e044565
mohamadou00@gmail.com <mohamadou00@gmail.com>
2021-12-03 21:45:04 +0000

    Initial commit

---
+++ src/main/java/com/mohamadou/springfooddeliveryorderapi/SpringFoodDeliveryOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohamadou.springfooddeliveryorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFoodDeliveryOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFoodDeliveryOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohamadou/springfooddeliveryorderapi/SpringFoodDeliveryOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamadou.springfooddeliveryorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoodDeliveryOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
