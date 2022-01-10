f2be4b1e84c67d73429e13889557831eeb0cae4f
mohamadou00@gmail.com <mohamadou00@gmail.com>
2022-01-10 14:21:21 +0000

    Test CategoryController

--- src/main/java/com/mohamadou/springfooddeliveryorderapi/controller/CategoryController.java
+++ src/main/java/com/mohamadou/springfooddeliveryorderapi/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.mohamadou.springfooddeliveryorderapi.entity.Category;
 import com.mohamadou.springfooddeliveryorderapi.request.CategoryRequest;
 import com.mohamadou.springfooddeliveryorderapi.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,27 +24,27 @@ public class CategoryController {
         this.categoryService  = categoryService;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    @GetMapping(path = "/{categoryId}")
+    @GetMapping(path = "/{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Category getCategoryById(@PathVariable Long categoryId) {
         return categoryService.getCategoryById(categoryId);
     }
 
-    @DeleteMapping(path = "/delete/{categoryId}")
+    @DeleteMapping(path = "/delete/{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public int deleteCategoryById(@PathVariable Long categoryId) {
         return categoryService.deleteCategoryById(categoryId);
     }
 
-    @PostMapping(path = "/create")
+    @PostMapping(path = "/create", produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Category createCategory(@RequestBody CategoryRequest categoryRequest) {
         return categoryService.createCategory(categoryRequest);
     }
 
-    @PutMapping(path = "/edit")
+    @PutMapping(path = "/edit", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Category updateCategory(@RequestBody CategoryRequest categoryRequest) {
         return categoryService.updateCategory(categoryRequest);
     }
